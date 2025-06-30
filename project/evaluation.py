8ebd2a3f0ed69760db0851cf9fc1bbb35a43efad
David Lelis <davidjlelis@gmail.com>
2025-06-29 21:54:58 -0400

    fixed pathfinding issue

--- project/evaluation.py
+++ project/evaluation.py
@@ -1,8 +1,12 @@
 import os
 import json
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 json_folder_path = '../videos/results/JSON/'
 results_data = []
+video_titles = []
+llm_results_per_video = [] # {'video_title': 'title', 'llm_responses': [response_1, response2, ..., responsex]}
 
 for filename in os.listdir(json_folder_path):
     if filename.endswith('.json'):
@@ -16,7 +20,13 @@ for filename in os.listdir(json_folder_path):
             except json.JSONDecodeError:
                 print(f'Warning: {filename} is not valid JSON')
 
-for result in results_data:
-    for i in result:
-        json_i = json.loads(i)
-        print(json_i['detection_id'], json_i['assistance_instructions'], '\n\n')
+
+
+for i in range(len(results_data)):
+    for j in range(len(results_data[i])):
+        json_result = json.loads(results_data[i][j])
+        if json_result[i][j]['video'] not in video_titles:
+            video_titles.append(json_result[i][j]['video'])
+            llm_results_per_video.append({'video_title': json_result[i][j]['video'], 'llm_responses': json_result[i][j]['assistance_instructions']})
+        else:
+            print(video_titles)
