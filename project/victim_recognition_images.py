93ceb06322440367665414d7f4d2d58d566a84e0
David Lelis <davidjlelis@gmail.com>
2025-10-08 17:48:43 -0400

    updated evaluations

--- project/victim_recognition_images.py
+++ project/victim_recognition_images.py
@@ -10,7 +10,7 @@
 
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import torch
 from transformers import AutoProcessor, AutoModelForCausalLM, AutoTokenizer, Pipeline
 from ultralytics import YOLO
@@ -48,15 +48,15 @@ except KeyError:
 
 client = InferenceClient(provider="nebius", api_key=hf_api)
 
-def is_emergency(description: str) -> bool:
+def is_emergency(description: str, message: str, environment_description: str) -> bool:
     prompt = f"""
-            You are a search-and-rescue assistant for first responders in a city after an earthquake.
+            You are a search-and-rescue assistant for first responders in the follow environment: {environment_description}.
 
             The description below is of an image and the message is from a caller requesting first responder assistance. 
             Combining the context of the image and the description, respond with a full description of the scene.
 
             Description: "{description}"
-            Message From Caller: "We are trapped by a building."
+            Message From Caller: "{message}"
 
             Respond with "1. Person Found" if and only if the description includes a person. If no preson is found, state "1. No Person Found"
             If so, include in the response "2. Person Requires Immediate Assistance" if and only if the person may be injured or in a dangerous situation based on the Description and Message From Caller. If they are not injured or in a dangerous situation, state "2. Person does not require assistance". 
@@ -78,98 +78,196 @@ def is_emergency(description: str) -> bool:
 
 # video_file_name = 'waterfall'
 # scene_path = './results/scenes/'
-img_path = './disaster_images_dataset/'
+img_folder_path = './images_dataset/sample/'
 img_results_path = '../videos/results/images/'
 json_results_path = '../videos/results/JSON/'
+# messages_fp = os.path.join(img_folder_path,'messages.json')
+
+with open('./images_dataset/messages.json', 'r') as m:
+    messages = json.load(m)
+
+# for i in messages:
+#     print(i['file_name'])
 
 best_conf = 0
+detection_id = 0
+detections_folder = './results/images_sim/'
 
-results_data = {"conf": 0.0, "person_found": False, "requires_assistance": False,
-                "assistance_instructions": '', "detection_id": False, "video": False}
+test = 'test_10'
 
-for folder in os.listdir(img_results_path):
-    print(folder)
+if not os.path.exists(detections_folder):
+    os.makedirs(detections_folder)
+results = []
 
-# for scene in os.listdir(scene_path):
-#     if scene.endswith('.jpg'):
-#         file_path = os.path.join(scene_path, scene)
+prev_vlm_desc = []
 
-#         scene_img = cv2.imread(file_path)
-#         # cv2.imshow('Image', scene_img)
-#         # cv2.waitKey(0)
-#         height, width, channels = scene_img.shape
+results_json_path = './results/images_sim/'
+results_json_filename = 'results.json'
+# results_json_filepath = os.path.join(results_json_path, results_json_filename)
+# with open(results_json_filepath, "w") as f:
+#     json.dump([], f)
 
-#         img_array = np.frombuffer(scene_img, dtype=np.uint8).reshape((height, width, channels))
-#         bgr_image = cv2.cvtColor(img_array, cv2.COLOR_BGRA2BGR) # YOLO
-#         rgb_image = cv2.cvtColor(img_array, cv2.COLOR_BGRA2RGB)
-#         pil_image = Image.fromarray(rgb_image)
+for file in os.listdir(img_folder_path):
+    fn = file.replace('.png', '')
+    fn = fn.replace('.jpg', '')
+    fn = fn.replace('.jpeg', '')
+    results_json_filepath = os.path.join(results_json_path+fn+'/', results_json_filename)
 
-#         json_file_name = f'{scene.replace(".jpg", "")}_results.json'
+    if test == 'test_1':
+        with open(results_json_filepath, "w") as f:
+            json.dump([], f)
+    '''
+    results files
+    {
+        'img_path' : '../../../
+        'world_and_scenario' : '..._file'
+        "conf": 0,
+        "person_found": true,
+        "vlm_description": "...",
+        "requires_assistance": false,
+        "assistance_instructions": "...",
+        "world_name": "image_dataset_human_damage",
+        "detection_id": 1,
+        "message": "We are trapped by a building."
+    }
+    '''
+    # print('Processing file', file)
+    results_data = {'img_path': img_folder_path+file
+                    , 'world_and_scenario': 'image_sim_'+file
+                    ,'conf': 0
+                    , 'person_found': False
+                    , 'vlm_description': None
+                    , 'requires_assistance': None
+                    , 'assistance_instructions': "image_dataset_human_damage"
+                    , 'detection_id': -1
+                    , 'message': None}
+    try:
+        img_path = os.path.join(img_folder_path, file)
+        image = cv2.imread(img_path, cv2.COLOR_BGRA2BGR)
+        pil_image = Image.open(img_path)
+    except UnidentifiedImageError:
+        print("Corrupt or unsupported file:", img_path)
 
-#         with open(f'{json_results_path}{json_file_name}', "w") as f:
-#             json.dump([], f)
+    # cv2.imshow("image",image)
+    # cv2.waitKey()
 
-#         # print(f'Processing frame {frame_number}')
-#         results = yolo_model(bgr_image, verbose=False)[0]
-#         person_detected = False
-#         annotated_frame = results.plot()
-#         # cv2.imshow('YOLOv8 Detection', annotated_frame)
-#         # cv2.waitKey(0)
+    max_conf = 0
+    best_result = None
 
-#         for result in results.boxes.data:
-#             x1, y1, x2, y2, conf, cls = result.tolist()
+    yolo_image_rotations = [image
+                            , cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+                            , cv2.rotate(image, cv2.ROTATE_180)
+                            , cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)]
 
-#             print(cls)
+    for i in range(len(yolo_image_rotations)):
+        results = yolo_model(yolo_image_rotations[i], verbose=False)[0]
+        for box in results.boxes:
+            cls = int(box.cls[0])
+            conf = float(box.conf[0])
+            if cls in (0,2) and conf > max_conf and conf > 0.8:
+                best_result = box
+                best_yolo_image = yolo_image_rotations[i]
+                max_conf = conf
 
-#             if int(cls) == 0:
-#                 if round(conf, 2) > 0.40:
-#                     person_detected = True
-#                     results_data['conf'] = conf
-#                     print('Person found! ', conf)
-#                     best_conf = 0
+                if i == 0:
+                    image_rotate_angle = 0
+                elif i == 1:
+                    image_rotate_angle = -90
+                elif i == 2:
+                    image_rotate_angle = 180
+                elif i == 3:
+                    image_rotate_angle = 90
+    
+    if best_result is not None:
+        x1, y1, x2, y2 = best_result.xyxy[0].tolist()
+        conf = float(best_result.conf[0])
+        cls = int(best_result.cls[0])
+        results = {}
+        if cls in (0, 2):
+            person_detected = True
+            results_data['conf'] = conf
 
-#         if person_detected:
-#             try:
-#             # Pass image through VLM to get description
-#                 prompt = "Describe the image"
-#                 inputs = VLM_processor(images=pil_image, text=prompt, return_tensors="pt").to(VL_model.device, torch_dtype)
-#                 output_tokens = VL_model.generate(**inputs, max_new_tokens=50)
-#                 generated_text = VLM_processor.batch_decode(output_tokens, skip_special_tokens=True)[0]
-#                 print(generated_text)
+            # set rotated image
+            cv2.rectangle(best_yolo_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+            if cls == 0:
+                cv2.putText(best_yolo_image, f"Person {conf:.2f}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
+            elif cls == 2:
+                cv2.putText(best_yolo_image, f"Car {conf:.2f}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
 
-#             # Pass descrption through LLM to get rescue guidance
-#                 LLM_response = is_emergency(generated_text)
-#                 print(LLM_response)
-#                 #insert into client response
-#                 results_data['person_found'] = True
-#                 results_data['requires_assistance'] = True if 'Person Found' in LLM_response else False
-#                 results_data['assistance_instructions'] = LLM_response
-#                 results_data['detection_id'] = f'{scene}'
-#                 results_data['video'] = scene
-#             except HfHubHTTPError as e:
-#                 if "402 Client Error" in str(e):
-#                     print('Max calls for free trier credits.')
-#                     continue
-#                 else:
-#                     raise
+            if image_rotate_angle == 0:
+                pass
+            elif image_rotate_angle == 90:
+                best_yolo_image = cv2.rotate(best_yolo_image, cv2.ROTATE_90_CLOCKWISE)
+            elif image_rotate_angle == 180:
+                best_yolo_image = cv2.rotate(best_yolo_image, cv2.ROTATE_180)
+            elif image_rotate_angle == -90:
+                best_yolo_image = cv2.rotate(best_yolo_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        if person_detected: # and self.far_from_other_detections(new_detection=(est_x, est_y), detections=detections) and (est_x, est_y) not in detections:            
+            # Florence2 VLM
+            prompt = "<MORE_DETAILED_CAPTION>"
+            inputs = VLM_processor(images=pil_image, text=prompt, return_tensors="pt").to(VL_model.device, torch_dtype)
+            output_tokens = VL_model.generate(**inputs, max_new_tokens=50)
+            vlm_description = VLM_processor.batch_decode(output_tokens, skip_special_tokens=True)[0]
+            vlm_description = vlm_description.replace('3D rendering', '')
+            detection_id += 1
+            if vlm_description not in prev_vlm_desc:
+                # print(f'VLM description: {vlm_description}')
 
-#             if results_data['person_found']:
-#                 cv2.putText(annotated_frame, f"Person found...", (10, 30),
-#                         cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 255), 1)
-                
-#                 # cv2.imwrite(f'{yolo_results_path}{video_file_name}_results_{frame_number}.jpg', annotated_frame)
-#                 cv2.imwrite(f'{img_results_path}{scene}_results.jpg', annotated_frame) 
-#                 results_json = json.dumps(results_data)
+                yolo_detection = f'{test}_{fn}.jpg'
+                yolo_detection_path = os.path.join(detections_folder+fn+'/', yolo_detection)
 
-#                 with open(f'{json_results_path}{json_file_name}', "r") as f:
-#                     data = json.load(f)
+                try:
+                    for item in messages:
+                        if item['file_name'] == file:
+                            message = item['message']
+                            environment_description = item['environment_description']
+                    results_data['message'] = message
+                    prev_vlm_desc.append(vlm_description)
+                    # LLM Processing
+                    LLM_response = is_emergency(description=vlm_description, message=message, environment_description=environment_description)
 
-#                 data.append(results_json)
+                    #insert into client response
+                    results_data['vlm_description'] = vlm_description
+                    results_data['person_found'] = True if '1. Person Found' in LLM_response else False
+                    results_data['requires_assistance'] = True if '2. Person Requires Assistance' in LLM_response else False
+                    results_data['assistance_instructions'] = LLM_response
+                except HfHubHTTPError as e:
+                    if "402 Client Error" in str(e):
+                        print('Max calls for free trier credits.')
 
-#                 with open(f'{json_results_path}{json_file_name}', "w") as f:
-#                     json.dump(data, f, indent=4)
+                        LLM_response = 'Max calls for free trier credits.'
+                        results_data['vlm_description'] = vlm_description
+                        results_data['person_found'] = True
+                        results_data['requires_assistance'] = 'N/A'
+                        results_data['assistance_instructions'] = LLM_response
+                        results_data['detection_id'] = detection_id
+                    else:
+                        print(f'Error occured {e}. Skipping image')
 
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break 
+                if results_data['person_found'] or person_detected:
+                    if best_yolo_image is None or best_yolo_image.size == 0:
+                        print("No valid image to save:", yolo_detection_path)
+                    else:
+                        cv2.imwrite(yolo_detection_path, best_yolo_image)
+                    cv2.putText(image, f"Person found...", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 255), 1)
+                    results_data['detection_id'] = detection_id
 
-# cv2.destroyAllWindows()
+
+                    # Export into results json file
+                    results_data = json.dumps(results_data)
+                    try:
+                        with open(results_json_filepath, "r") as f:
+                            data = json.load(f)
+
+                        data.append(results_data)
+
+                        with open(results_json_filepath, "w") as f:
+                            json.dump(data, f, indent=4)
+                        # print(f'Data sent to server: {telemetry_json}')
+                    except BrokenPipeError:
+                        print("⚠️ Connection lost while sending data. Skipping send.")
+                        break  # Exit the run() loop if server is gone
+    # cv2.imshow('Image', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
